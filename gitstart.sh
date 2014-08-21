33db71d9131837345b81b71f82e85d3065070e3f
x86nick <x86nick@gmail.com>
2014-08-20 22:46:24 -0700

    Create gitstart.sh

---
+++ gitstart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+working=$1
+repo_path=$2
+
+echo ; [ "$#" -lt 2 ] && { echo "Usage: $0 git_working_dir git_repo_path"; echo; exit 1; }
+
+cd "${repo_path}"
+git checkout master
+git pull
+git branch -D "${working}"
+git branch "${working}"
+git checkout "${working}"
+
+cd "${repo_path}"
+
+git branch
+
+echo ""
+echo "Now you are ready to start making your changes"
+echo ""
+echo "Once you are done with your changes\n do 'git add .' , git commit -m "stuff i like to change." , git push origin devarshi_working\n and then go to magneto site to merge changes\n"
+echo "Or just run gitMerge.sh"
+echo ""
